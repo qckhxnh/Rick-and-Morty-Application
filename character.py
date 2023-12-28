bcf413d7ae49e9216041a944d5c123d45a16947e
qckhxnh <khanh.kieu@student.lut.fi>
2023-12-28 16:03:58 +0200

    git commit -m "final commit"

--- character.py
+++ character.py
@@ -1,5 +1,12 @@
+import io, requests
+from PIL import Image
+import os.path
+
+
 class Character:
-    def __init__(self, name, gender, species, origin, status, image, number_of_episodes):
+    def __init__(self, name, gender, species,
+                 origin, status,
+                 image, number_of_episodes):
         self.name = name
         self.gender = gender
         self.species = species
@@ -7,6 +14,17 @@ class Character:
         self.status = status
         self.image_url = image
         self.number_of_episodes = number_of_episodes
+        self.image_path ='./images/'+self.name.replace(' ', '')+'.png'
+        self.download_image()
+
+    def download_image(self):
+        if not os.path.exists(self.image_path):
+            response = requests.get(self.image_url)
+            img_data = response.content
+            image = Image.open(io.BytesIO(img_data))
+            resized_image = image.resize((200, 200), Image.ANTIALIAS)
+            resized_image.save(self.image_path)
+            print(self.name + ' Image is Downloaded!')
     
-    def show_character(self):
-        print(self.name, self.gender)
+    def get_image(self):
+        return Image.open(self.image_path)
--- main.py
+++ main.py
@@ -22,7 +22,6 @@ def load_data():
             image_url = obj['image']
             number_of_episodes = len(obj['episode'])
             character = Character(name, gender, species, origin, status, image_url, number_of_episodes)
-            character.show_character()
             characters.append(character)
     return characters
 
@@ -32,27 +31,35 @@ char = characters[0]
 #Creating UI
 root = Tk()
 root.title("Rick and Morty")
-root.geometry("535x560")
+root.geometry("500x560")
 root.update()
-root.resizable(0,0)
 scrollable = ScrollableFrame(root)
 scrollable.pack(fill = BOTH, expand= True)
 
-#Frame for each character
-list_item_frame = Frame(scrollable.scrollable_frame, borderwidth=4, relief= GROOVE)
-list_item_frame.pack(fill = X, padx = 7.5)
+for char in characters:
+    #Frame for each character
+    list_item_frame = Frame(scrollable.scrollable_frame, borderwidth=6, relief= GROOVE)
+    list_item_frame.pack(fill = X, padx = 7.5)
 
-#Left frame
-left_frame = Frame(list_item_frame)
-left_frame.grid(row=0, column=0, padx = 7.5, pady = 15)
+    #Left frame
+    left_frame = Frame(master=list_item_frame)
+    image = ImageTk.PhotoImage(char.get_image())
+    image_lbl = Label(left_frame, image=image)
+    image_lbl.image = image
+    image_lbl.pack(fill=BOTH, expand=True)
+    left_frame.grid(row=0, column=0, padx=7.5, pady=15) 
 
-#Load the image
+    #Right frame
+    right_frame = Frame(list_item_frame)
+    right_frame.grid(row=0, column=1, sticky= "we")
+    Label(right_frame, text = "Name: " +char.name, font=("Helvetica", 12), padx = 7.5).pack(anchor = W, expand = True)
+    Label(right_frame, text = "Gender: " +char.gender, font=("Helvetica", 12), padx = 7.5).pack(anchor = W, expand = True)
+    Label(right_frame, text = "Species: " +char.species, font=("Helvetica", 12), padx = 7.5).pack(anchor = W, expand = True)
+    Label(right_frame, text = "Origin: " +char.origin, font=("Helvetica", 12), padx = 7.5).pack(anchor = W, expand = True)
+    Label(right_frame, text = "Status: " +char.status, font=("Helvetica", 12), padx = 7.5).pack(anchor = W, expand = True)
+    Label(right_frame, text = "Number of episodes: " +str(char.number_of_episodes), font=("Helvetica", 12), padx = 7.5).pack(anchor = W, expand = True)
 
-#Right frame
-right_frame = Frame(list_item_frame)
-right_frame.grid(row=0, column=1, sticky= "we")
-Label(right_frame, text = "Name: " +char.name, font=("Helvetica", 12), padx = 7.5).pack(anchor = W, expand = True)
-
+#Main loop
 root.mainloop()
 
 
